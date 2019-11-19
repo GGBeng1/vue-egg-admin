1c681e4e35168b5792bfa3f1a1e35743d90f2733
ggbeng <15010976823@163.com>
2019-11-19 16:33:57 +0800

    :car: 增加vuex持久化插件

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import mutations from './mutations'
-import state from './state'
-
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import mutations from "./mutations";
+import state from "./state";
+import VuexPersistence from "vuex-persist";
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  reducer: state => {
+    return {
+      tabsList: state.tabsList,
+      defaultActive: state.defaultActive
+    };
+  }
+});
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state,
-  mutations
-})
+  mutations,
+  plugins: [vuexLocal.plugin]
+});
